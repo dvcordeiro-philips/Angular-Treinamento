3986e7a2ce9d3079930557b9ed5c0c134fa79b08
Diego Valdemir Cordeiro <diego.valdemir.cordeiro.com>
2016-08-11 14:54:06 -0300

    Multiparametros em AngularJS

---
+++ 02/multiparam.controller.js
@@ -0,0 +1,14 @@
+angular.module("Aplicacao", [])
+    .controller("control", function ($scope) {
+       $scope.cadastrar = cadastrar;
+       $scope.contatos = [];
+
+       function cadastrar(contato) {
+           $scope.contatos.push({
+               nome : contato.nome,
+               idade : contato.idade,
+               telefone : contato.telefone
+           });
+           console.log($scope.contatos);
+       }
+    });
